55fbb49d249876f7e461d126253287d628c7e3b9
bam <juhyun19981129@gmail.com>
2023-05-09 19:34:08 +0900

    초기 공사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import {useState} from 'react';
+
 const App = () => {
-  return;
+  let [color, setColor] = useState();
+
+  color = {
+    color: '000000',
+    text: 'Thumbnail Maker',
+    href: '',
+  }
+
+  return (
+    <></>
+  );
 };
 
 export default App;
