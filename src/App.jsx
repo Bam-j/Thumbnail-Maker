3d20a9dd181e3eee25a7943345b558ef530cd0b1
bam <juhyun19981129@gmail.com>
2023-05-31 22:45:14 +0900

    css -> scss 교체

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Header } from "./components/Header";
 import { ThumbnailPalette } from "./components/contents/ThumbnailPalette";
 import { TextInput } from "./components/contents/TextInput";
+import './styles/App.scss';
 
 const App = () => {
   return (
     <>
       <Header />
-      <div>
+      <div className={'maker-container'}>
         <ThumbnailPalette/>
         <TextInput/>
       </div>
