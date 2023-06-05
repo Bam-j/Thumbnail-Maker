c2113e55ca1c3883f5860fe3d95b10778d7c08a7
bam <juhyun19981129@gmail.com>
2023-06-05 21:57:40 +0900

    컬러 픽커 개조

--- src/components/Buttons/ColorPickerButtons/FontColorPickerButton.jsx
+++ src/components/Buttons/ColorPickerButtons/FontColorPickerButton.jsx
@@ -1,15 +1,25 @@
 import { BiFontColor } from 'react-icons/bi';
 import styled from 'styled-components';
+import { useState } from "react";
+import { ColorPicker } from "../../Tools/ColorPicker";
 
 /*
 기능: 폰트를 선택하면 Preview의 글자 색 변경. 컬러 선택기가 오픈.
  */
 export const FontColorPickerButton = () => {
+  const [isClick, setIsClick] = useState(false);
+
+  const onClick = () => setIsClick(!isClick);
+
   return (
-    <div>
+    <div className={'ColorPickerWrapper'}>
       <Button>
         <BiFontColor />
       </Button>
+      {
+        isClick &&
+        <ColorPicker/>
+      }
     </div>
   );
 };
--- src/components/Tools/ColorPicker.jsx
+++ src/components/Tools/ColorPicker.jsx
@@ -1,44 +1,101 @@
-import { CustomPicker } from "react-color";
-import { EditableInput, Hue, Saturation } from "react-color/lib/components/common";
+import { CustomPicker } from 'react-color';
+import {
+  EditableInput,
+  Hue,
+  Saturation,
+} from 'react-color/lib/components/common';
 /*
 기능: 폰트와 배경색 버튼을 누르면 나타날 색상 선택기.
  */
 
 export const ColorPicker = ({ hex, hsl, hsv, onChange }) => {
   const styles = {
-    hue: {
-      height: 10,
-      position: "relative",
-      marginBottom: 10
+    body: {
+      borderRadius: '14px',
+      width: '200px',
+      backgroundColor: '#fff',
+      transition: `visibility 0s, opacity 0.5s linear`,
+      boxShadow: '0px 0px 30px 5px rgba(0, 0, 0, 0.50)',
+    },
+    Saturation: {
+      borderRadius: '5px 5px 0 0',
     },
     saturation: {
-      width: 100,
-      height: 100,
-      position: "relative"
-    },
-    input: {
-      height: 34,
-      border: `1px solid ${hex}`,
-      paddingLeft: 10
+      width: '100%',
+      paddingBottom: '55%',
+      position: 'relative',
+      borderRadius: '2px 2px 0 0',
+      overflow: 'hidden',
     },
     swatch: {
-      width: 54,
-      height: 38,
-      background: hex
-    }
+      marginTop: '6px',
+      marginBottom: '5px',
+      width: '16px',
+      height: '16px',
+      borderRadius: '8px',
+      position: 'relative',
+      overflow: 'hidden',
+    },
+    hue: {
+      width: '200px',
+      height: ' 10px',
+      position: 'relative',
+    },
+    triangle: {
+      width: '0px',
+      height: '0px',
+      borderStyle: 'solid',
+      borderWidth: '10px 10px 10px 10px',
+      borderColor: `#fff transparent transparent transparent`,
+      position: 'absolute',
+      left: '50%',
+      marginLeft: '-10px',
+    },
+    squareShadow: {
+      position: 'absolute',
+      width: '200px',
+      top: '10px',
+      height: '150px',
+    },
+    roundShadow: {
+      position: 'absolute',
+      top: '100px',
+      width: '200px',
+      height: '78px',
+      borderRadius: '12px',
+      backgroundColor: 'white',
+    },
+    editableInput: {
+      input: {
+        border: 'none',
+        textAlign: 'center',
+      },
+    },
   };
+
   return (
     <div>
       <div style={styles.hue}>
-        <Hue hsl={hsl} onChange={onChange} />
+        <Hue
+          hsl={hsl}
+          onChange={onChange}
+        />
       </div>
 
       <div style={styles.saturation}>
-        <Saturation hsl={hsl} hsv={hsv} onChange={onChange} />
+        <Saturation
+          hsl={hsl}
+          hsv={hsv}
+          onChange={onChange}
+        />
       </div>
 
-      <div style={{ display: "flex" }}>
-        <EditableInput style={{ input: styles.input }} value={hex} onChange={onChange} />
+      <div style={{ display: 'flex' }}>
+        <EditableInput
+          style={{ input: styles.input }}
+          value={hex}
+          onChange={onChange}
+        />
         <div style={styles.swatch} />
       </div>
     </div>
