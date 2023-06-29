f969237923840764c17aea0610f5d8a6a3f02b26
bam <juhyun19981129@gmail.com>
2023-06-29 14:43:07 +0900

    불필요 코드 삭제

--- src/contexts/thumbnailContents.js
+++ src/contexts/thumbnailContents.js
@@ -5,12 +5,10 @@ const ThumbnailContentsContext = createContext({
     width: '',
     height: '',
     text: '',
-    typeOfColorPicker: '',
     textColor: '',
     fontSize: '',
     fontFamily: '',
     backgroundColor: '',
-    href: '',
   },
   actions: {},
 });
@@ -19,35 +17,29 @@ const ThumbnailContentsProvider = ({ children }) => {
   const [width, setWidth] = useState(700);
   const [height, setHeight] = useState(350);
   const [text, setText] = useState('Sample');
-  const [typeOfColorPicker, setTypeOfColorPicker] = useState('');
   const [textColor, setTextColor] = useState('#ffffff');
   const [fontSize, setFontSize] = useState(60);
   const [fontFamily, setFontFamily] = useState('Helvetica');
   const [backgroundColor, setBackgroundColor] = useState('#000000');
-  const [href, setHref] = useState('');
 
   const value = {
     state: {
       width,
       height,
       text,
-      typeOfColorPicker,
       textColor,
       fontSize,
       fontFamily,
       backgroundColor,
-      href,
     },
     actions: {
       setWidth,
       setHeight,
       setText,
-      setTypeOfColorPicker,
       setTextColor,
       setFontSize,
       setFontFamily,
       setBackgroundColor,
-      setHref,
     },
   };
 
